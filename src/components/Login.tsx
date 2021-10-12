96c078982d04719ccbd156c140323e3db9f254a0
princecyrelle <princedevera10@yahoo.com>
2021-10-12 23:15:07 +0900

    login

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -3,20 +3,36 @@ import React, { useState } from "react"
 function Login() {
   const usernameref = React.useRef<HTMLInputElement>(null)
   const passwordref = React.useRef<HTMLInputElement>(null)
+  const [loggedIn, setLoggedIn] = useState<boolean>(false)
 
   const onSubmitHandler = () => {
-    const datas = {
+    const datas: { [key: string]: any } = {
       username: usernameref.current?.value,
       password: passwordref.current?.value,
     }
-    let responsedata
+    var formBody = []
+    for (var property in datas) {
+      var encodedKey = encodeURIComponent(property)
+      var encodedValue = encodeURIComponent(datas[property])
+      formBody.push(encodedKey + "=" + encodedValue)
+    }
+
     fetch("http://localhost:8001/login", {
       method: "post",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: JSON.stringify(datas),
-    }).then((data) => {
-      console.log(data.json())
+      body: formBody.join("&"),
     })
+      .then((data) => {
+        return data.json()
+      })
+      .then((result) => {
+        if (result.hasOwnProperty("token")) {
+          window.localStorage.setItem("xauthToken", result.token)
+          setLoggedIn(true)
+        } else {
+          console.log("ERROR")
+        }
+      })
   }
   return (
     <div className="logincontainer">
